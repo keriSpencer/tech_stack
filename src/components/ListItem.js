c0d6752243b3efa957db73d39cc162f2c94530a6
Keri Spencer <keri.spencer@redvector.com>
2019-02-27 14:12:12 -0500

    Add description expansion and layout animation spring.

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -1,10 +1,33 @@
 import React, { Component } from 'react';
-import { Text, TouchableWithoutFeedback, View } from 'react-native';
+import {
+  Text,
+  TouchableWithoutFeedback,
+  View,
+  LayoutAnimation
+} from 'react-native';
 import { connect } from 'react-redux';
 import { CardSection } from './common';
 import * as actions from '../actions';
 
 class ListItem extends Component {
+  componentWillUpdate() {
+    LayoutAnimation.spring();
+  }
+
+  renderDescription() {
+    const { library, expanded } = this.props;
+
+    if (expanded) {
+      return (
+        <CardSection>
+          <Text style={{ flex: 1 }}>
+            {library.description}
+          </Text>
+        </CardSection>
+      );
+    }
+  }
+
   render() {
     const { titleStyle } = styles;
     const { title, id } = this.props.library;
@@ -13,12 +36,13 @@ class ListItem extends Component {
       <TouchableWithoutFeedback
         onPress={() => this.props.selectLibrary(id)}
       >
-        <View>
+        <View style={styles.viewStyles}>
           <CardSection>
             <Text style={titleStyle}>
               {title}
             </Text>
           </CardSection>
+          {this.renderDescription()}
         </View>
       </TouchableWithoutFeedback>
     )
@@ -28,8 +52,17 @@ class ListItem extends Component {
 const styles = {
   titleStyle: {
     fontSize: 18,
-    paddingLeft: 15
+    paddingLeft: 15,
+  },
+  viewStyles: {
+    backgroundColor: 'white'
   }
 }
 
-export default connect(null, actions)(ListItem);
+const mapStateToProps = (state, ownProps) => {
+  const expanded = state.selectedLibraryId === ownProps.library.id;
+
+  return { expanded };
+};
+
+export default connect(mapStateToProps, actions)(ListItem);
